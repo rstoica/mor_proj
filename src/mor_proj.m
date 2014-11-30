3ce2f156867ccf14aa310a8cc6918ba94db84efd
Andrei Stoica <stoica.razvan.andrei@gmail.com>
2014-12-01 00:25:20 +0100

    Changed main script to changes in solve_sys

--- src/mor_proj.m
+++ src/mor_proj.m
@@ -58,16 +58,18 @@ end
 
 % generate the C matrix (see project report for details)
 sys.C = sys.B(:,1:7)';
-
 % get poles of the system
 sys = anal_sys(sys);
 
+tol1 = 10^-2;
+tol1 = 0.5*10^-2;
+tol1 = 10^-3;
+
 % model reduction using balancing transformation (using Grammians) 
-tol = 10^-2;
-[sys_bal, res_bal] = bal_reduce(sys, tol);
+[sys_bal, res_bal] = bal_reduce(sys, tol1);
 
 % model reduction using modal approximation (diagonalization of A matrix)
-[sys_mod, res_mod] = mod_reduce(sys, tol);
+[sys_mod, res_mod] = mod_reduce(sys, tol1);
 
 % plot of the normalized Hankel singular values (normalization done with
 % respect to the highest sv) and of the modal coefficients (normalization 
@@ -85,139 +87,57 @@ legend('Hankel normaliz. sv','Modal coeff.');
 xlim([1 sys.n]);
 grid on
 
-% poles of the system
+% poles of the systems
 poles_plot = [real(sys.poles),imag(sys.poles)];
-mins = [min(poles_plot(:,1)),min(poles_plot(:,2))];
-maxs = [max(poles_plot(:,1)),max(poles_plot(:,2))];
 bal_poles_plot = [real(sys_bal.poles),imag(sys_bal.poles)];
-mins = [mins;min(bal_poles_plot(:,1)),min(bal_poles_plot(:,2))];
-maxs = [maxs;max(bal_poles_plot(:,1)),max(bal_poles_plot(:,2))];
 mod_poles_plot = [real(sys_mod.poles),imag(sys_mod.poles)];
-mins = [mins;min(mod_poles_plot(:,1)),min(mod_poles_plot(:,2))];
-maxs = [maxs;max(mod_poles_plot(:,1)),max(mod_poles_plot(:,2))];
 pfig = figure(2);
 hold on
 grid on 
 plot(poles_plot(:,1),poles_plot(:,2),'ko','MarkerSize',12,'LineWidth',2,'MarkerFaceColor','b');
 plot(bal_poles_plot(:,1),bal_poles_plot(:,2),'ko','MarkerSize',8,'LineWidth',2,'MarkerFaceColor','r');
 plot(mod_poles_plot(:,1),mod_poles_plot(:,2),'ko','MarkerSize',8,'LineWidth',2,'MarkerFaceColor','y');
-if (max(maxs(:,1)) > 0.5)
-    max_x = max(maxs(:,1));
-else
-    max_x = 0.5;
-end
 legend('Original Sys','Bal. Trunc. System','Mod. Trunc. System','Location','NorthWest');
-plot(zeros(1,50),linspace(min(mins(:,2)),max(maxs(:,2)),50),'k--');
-plot(linspace(min(mins(:,1)),max_x,50),zeros(1,50),'k--');
-xlim([min(mins(:,1)) max_x]);
-ylim([min(mins(:,2)) max(maxs(:,2))]);
+plot(zeros(1,50),linspace(-100,0.5,50),'k--');
+plot(linspace(-2.5,2.5,50),zeros(1,50),'k--');
+xlim([-100 0.5]);
+ylim([-2 2]);
 pause()
-xlim([-4 max_x]);
+xlim([-4 0.5]);
 
-% step response of the original system
-t = linspace(0,40,12500)';
-[y_o,~,x_o] = step_sys(sys,t);
-in = size(y_o,3);
-inputs = {['1=I1'],['2=I3'],['3=I5'],['4=I7'],['5=I9'],['6=I11'],['7=I13'],['8=VDD']};
-stfig_o = figure(3);
-title('Step response of the original system');
-hold on
-for i = 1:in
-    subplot(2,4,i), plot(t,y_o(:,:,i));
-    legend('VC1','VC3','VC5','VC7','VC9','VC11','VC13')
-    title(['Unit step response for input ',inputs(i)]);
-    xlabel('seconds')
-    ylabel('amplitude (V)')
-end
-
-% step response of the balanced truncated system
-[y_b,t_b,x_b] = step_sys(sys_bal,t);
-in = size(y_b,3);
-stfig_b = figure(4);
-title('Step response of the balanced reduced system');
-hold on
-for i = 1:in
-    subplot(2,4,i), plot(t,y_b(:,:,i));
-    legend('VC1','VC3','VC5','VC7','VC9','VC11','VC13')
-    title(['Unit step response for input ',inputs(i)]);
-    xlabel('seconds')
-    ylabel('amplitude (V)')
-end
-
-% step response of the modal approximated system
-[y_m,~,x_m] = step_sys(sys_mod,t);
-in = size(y_m,3);
-inputs = {['1=I1'],['2=I3'],['3=I5'],['4=I7'],['5=I9'],['6=I11'],['7=I13'],['8=VDD']};
-stfig_c = figure(5);
-title('Step response of the modal approximated system');
-hold on
-for i = 1:in
-    subplot(2,4,i), plot(t,y_m(:,:,i));
-    legend('VC1','VC3','VC5','VC7','VC9','VC11','VC13')
-    title(['Unit step response for input ',inputs(i)]);
-    xlabel('seconds')
-    ylabel('amplitude (V)')
-end
-
-% error systems plots
-stfig_err = figure(6);
-for i = 1:in
-    subplot(2,4,i), plot(t,y_o(:,:,i)-y_m(:,:,i),'b-');
-    hold on
-    plot(t,y_o(:,:,i)-y_b(:,:,i),'r-');
-    title(['Error in step response for input ',inputs(i),' at outputs']);
-    xlabel('seconds')
-    ylabel('amplitude (V)')
-end
-
-% Bode plot for the outputs
-bf = logspace(-3,3,60);
-[bamp, ~] = sigma_sys(sys,bf);
-[bamp_bal, ~] = sigma_sys(sys_bal,bf);
-[bamp_mod, ~] = sigma_sys(sys_mod,bf);
-bfig = figure(7);
-semilogx(bf,20*log10(bamp(1:size(bamp,1),:)),'*-');
-legend('VCap1','VCap3','VCap5','Vcap7','VCap9','Vcap11','Vcap13')
-grid on
-set(gca,'xscale','log');
-title('Bode plot of system outputs - Original system')
-xlabel('Frequency (rad/s)');
-ylabel('Amplitude (dB)');
-
-bfig_b = figure(8);
-semilogx(bf,20*log10(bamp_bal(1:size(bamp_bal,1),:)),'*-');
-legend('VCap1','VCap3','VCap5','Vcap7','VCap9','Vcap11','Vcap13')
-grid on
-set(gca,'xscale','log');
-title('Bode plot of system outputs - Balanced red.')
-xlabel('Frequency (rad/s)');
-ylabel('Amplitude (dB)');
-
-bfig_m = figure(9);
-semilogx(bf,20*log10(bamp_mod(1:size(bamp_mod,1),:)),'*-');
-legend('VCap1','VCap3','VCap5','Vcap7','VCap9','Vcap11','Vcap13')
-grid on
-set(gca,'xscale','log');
-title('Bode plot of system outputs - Modal approx.')
-xlabel('Frequency (rad/s)');
-ylabel('Amplitude (dB)');
-
-bfig_err = figure(10);
-hold on
-semilogx(bf,20*log10(bamp(1:size(bamp,1),:))-20*log10(bamp_bal(1:size(bamp_bal,1),:)),'b');
-semilogx(bf,20*log10(bamp(1:size(bamp,1),:))-20*log10(bamp_mod(1:size(bamp_mod,1),:)),'r');
-grid on
-set(gca,'xscale','log');
-title('Bode plot of error systems')
-xlabel('Frequency (rad/s)');
-ylabel('Amplitude (dB)');
-
-% error norms 
+t = linspace(0,40,10000)';
+f = logspace(-3,3,60);
+% step response and Bode plot of the systems
 % original system
-sys_hinf = normhinf(sys.A,sys.B,sys.C,sys.D);
-sys_b_hinf = normhinf(sys_bal.A,sys_bal.B,sys_bal.C,sys_bal.D);
-sys_m_hinf = normhinf(sys_mod.A,sys_mod.B,sys_mod.C,sys_mod.D);
+[sys, sol_sys] = solve_sys(sys,t,f);
+[sys_bal, sol_sys_bal] = solve_sys(sys_bal,t,f);
+[sys_mod, sol_sys_mod] = solve_sys(sys_mod,t,f);
 
-sys_h2 = normh2(sys.A,sys.B,sys.C,sys.D);
-sys_b_h2 = normh2(sys_bal.A,sys_bal.B,sys_bal.C,sys_bal.D);
-sys_m_h2 = normh2(sys_mod.A,sys_mod.B,sys_mod.C,sys_mod.D);
+error_norms.bal_hinf = norm(sol_sys.hinf-sol_sys_bal.hinf);
+error_norms.bal_h2 = norm(sol_sys.h2-sol_sys_bal.h2);
+error_norms.mod_hinf = norm(sol_sys.hinf-sol_sys_mod.hinf);
+error_norms.mod_h2 = norm(sol_sys.h2-sol_sys_mod.h2);
+error_norms.bal_hinf_bound = 2*sum(res_bal.hsv(res_bal.k+1:end));
+
+disp(error_norms)
+
+% % error systems plots: step and Bode plot
+% stfig_err = figure(6);
+% for i = 1:in
+%     subplot(2,4,i), plot(t,y_o(:,:,i)-y_m(:,:,i),'b-');
+%     hold on
+%     plot(t,y_o(:,:,i)-y_b(:,:,i),'r-');
+%     title(['Error in step response for input ',inputs(i),' at outputs']);
+%     xlabel('seconds')
+%     ylabel('amplitude (V)')
+% end
+% 
+% bfig_err = figure(10);
+% hold on
+% semilogx(bf,20*log10(bamp(1:size(bamp,1),:))-20*log10(bamp_bal(1:size(bamp_bal,1),:)),'b');
+% semilogx(bf,20*log10(bamp(1:size(bamp,1),:))-20*log10(bamp_mod(1:size(bamp_mod,1),:)),'r');
+% grid on
+% set(gca,'xscale','log');
+% title('Bode plot of error systems')
+% xlabel('Frequency (rad/s)');
+% ylabel('Amplitude (dB)');
