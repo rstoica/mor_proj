9c7ecdc5f7ddf197053f58419e92109bb15f8c03
Andrei Stoica <stoica.razvan.andrei@gmail.com>
2014-11-30 03:48:58 +0100

    Added more input params to wrappers to enhance functionality

--- src/sigma_sys.m
+++ src/sigma_sys.m
@@ -1,10 +1,17 @@
-function [ ] = sigma_sys( sys )
+function [ SV, W ] = sigma_sys( sys, w )
 % sigma_sys Wrapper for sigma MATLAB function from dependency toolbox
 % Dynamical Systems. This is a light weight implementation, allowing just
 % the sys parameter to be passed. This yields the Bode plot of the SV of 
 % the system. For more details type "help sigma"
 
     sys_dummy = ss(sys.A,sys.B,sys.C,sys.D);
-    sigma(sys_dummy);
+    switch nargin
+        case 2
+            [SV, W] = sigma(sys_dummy,w);
+        case 1
+            [SV, W] = sigma(sys_dummy);
+        otherwise
+            assert(0);
+    end
 end
 
--- src/step_sys.m
+++ src/step_sys.m
@@ -1,9 +1,16 @@
-function [ Y, T, X, YSD ] = step_sys( sys )
+function [ Y, T, X, YSD ] = step_sys( sys , t )
 % step_sys Wrapper for the step function from MATLAB dynamical systems
 % toolbox which needs to be installed as dependency. For more details, type
 % "help step"
 
     sys_dummy = ss(sys.A, sys.B, sys.C, sys.D);
-    [Y, T, X, YSD] = step(sys_dummy);
+    switch nargin
+        case 2
+            [Y, T, X, YSD] = step(sys_dummy, t);
+        case 1
+            [Y, T, X, YSD] = step(sys_dummy);
+        otherwise
+            assert(0);
+    end
 end
 
